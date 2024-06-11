b4ee86fbca07f0761096692c2385a2b72afcda87
AkhileshBThapa <akhileshbikramthapa8@gmail.com>
2024-06-11 19:29:18 -0400

    Final update
    Add comments

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,12 @@
+//Assign your name and studentNumber to a variable called studentName and studentNumber
 const studentName = 'Akhilesh Bikram Thapa';
 const studentNumber = '1172618';
 
+//combine the variables into a single string printed into the console log
 const result = (studentName).concat('-', studentNumber);
 console.log(result);
 
-const headrContent =document.querySelector("h1");
+//Use document.querySelector to access the <h1> and assign it to a variable called headerContent
+const headrContent=document.querySelector("h1");
 headrContent.innerHTML=result;
 headrContent.classList.add("headings");
-
