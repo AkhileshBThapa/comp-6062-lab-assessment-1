7cb0875f184204e6672e3ec83a18ed93916f9ecb
AkhileshBThapa <akhileshbikramthapa8@gmail.com>
2024-06-11 19:09:01 -0400

    Lab
    Lab

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+console.log('Akhilesh Bikram Thapa');
+const studentName = 'Akhilesh Bikram Thapa';
+const studentNumber = '1172618';
+
+const result = (studentName).concat('-', studentNumber);
+console.log(result);
+
+const headrContent =document.querySelector("id");
+headrContent.innerHTML=result;
+headrContent.classList.add("headings");
+
